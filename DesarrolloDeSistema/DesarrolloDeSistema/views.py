afbdb2995c8c8dfe9deaedf9c1792f8d7b94b452
EricG <ericrivas192@gmail.com>
2023-11-22 09:05:22 -0600

    programacion en el front

--- DesarrolloDeSistema/DesarrolloDeSistema/views.py
+++ DesarrolloDeSistema/DesarrolloDeSistema/views.py
@@ -11,7 +11,9 @@ def parametro(request, edad):
 
 def plantillas(request):   # funcion que llama a plantillas (no es la más optima)
 
-    fecha_del_dia_de_hoy = date.today().day
+    fecha_del_dia_de_hoy = date.today()
+
+    materias = ["Matematicas", "Ciencias Narurales", "Ciencias Sociales"]
 
     doc_externo = open("C:/Documentos/DESSARROLLO DE SOFTWARE/django/pruebas django y github/DesarrolloDeSistema/plantillas/index.html") # abrimos el documento html para esta funcion
 
@@ -19,9 +21,9 @@ def plantillas(request):   # funcion que llama a plantillas (no es la más optim
 
     doc_externo.close()  # cerramos el html porque ya esta guardado
 
-    contexto = Context({"nombre_del_profesor": "Mario", "apellido_del_profesor": "Enriquez", "fecha_actual": fecha_del_dia_de_hoy})  # por ahora no sirve ya que el html es simple
+    contexto = Context({"nombre_del_profesor": "Mario", "apellido_del_profesor": "Enriquez", "fecha_actual": fecha_del_dia_de_hoy, "materias": materias})  # por ahora no sirve ya que el html es simple
 
-    documento = plantilla.render(contexto)
+    documento = plantilla.render(contexto)  # le pasamos los cambios que realiza python sobre html
 
-    return HttpResponse(documento)
+    return HttpResponse(documento)  # mostamos la plantilla 
 
