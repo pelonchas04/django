1cf6f1f155760a9cef702897b7efb6b0b26c3253
EricG <ericrivas192@gmail.com>
2023-11-21 19:23:11 -0600

    maneja entre front y back

--- DesarrolloDeSistema/DesarrolloDeSistema/views.py
+++ DesarrolloDeSistema/DesarrolloDeSistema/views.py
@@ -1,6 +1,7 @@
 # funciones a mostrar
 from django.http import HttpResponse
 from django.template import Template, Context
+from datetime import date
 
 def mostrar(request):
     return HttpResponse("<html><body><h1>Hello Elias!</h1></body></html>")
@@ -10,13 +11,15 @@ def parametro(request, edad):
 
 def plantillas(request):   # funcion que llama a plantillas (no es la más optima)
 
+    fecha_del_dia_de_hoy = date.today().day
+
     doc_externo = open("C:/Documentos/DESSARROLLO DE SOFTWARE/django/pruebas django y github/DesarrolloDeSistema/plantillas/index.html") # abrimos el documento html para esta funcion
 
     plantilla = Template(doc_externo.read()) # cargamos el documento (ya esta listo)
 
     doc_externo.close()  # cerramos el html porque ya esta guardado
 
-    contexto = Context()  # por ahora no sirve ya que el html es simple
+    contexto = Context({"nombre_del_profesor": "Mario", "apellido_del_profesor": "Enriquez", "fecha_actual": fecha_del_dia_de_hoy})  # por ahora no sirve ya que el html es simple
 
     documento = plantilla.render(contexto)
 
